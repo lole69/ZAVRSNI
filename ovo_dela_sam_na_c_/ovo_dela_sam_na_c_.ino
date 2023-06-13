e261bf83f4fae4dfdc9888c61a181544c77769c1
lole69 <102100407+lole69@users.noreply.github.com>
2023-06-14 01:15:51 +0200

    Add files via upload

---
+++ ovo_dela_sam_na_c_/ovo_dela_sam_na_c_.ino
@@ -0,0 +1,47 @@
+
+
+#include <SoftwareSerial.h>
+SoftwareSerial myConnection(10, 11);
+
+
+int pushButton = 4;
+int pushButton2 = 6;
+int pushButton3 = 9;
+int pushButton4 = 2;
+
+void setup() {
+  Serial.begin(9600);
+  myConnection.begin(9600);
+  pinMode(pushButton, INPUT);
+}
+
+void loop() {
+  if (digitalRead(pushButton) == HIGH) {
+    myConnection.println("g1");
+    //myConnection.println("\n");
+    Serial.flush();
+  }
+  if (digitalRead(pushButton2) == HIGH) {
+    myConnection.println("d1");
+    //myConnection.println("\n");
+    Serial.flush();
+
+  }
+  if (digitalRead(pushButton3) == HIGH) {
+    myConnection.println("g2");
+    //myConnection.println("\n");
+    Serial.flush();
+  }
+  if (digitalRead(pushButton4) == HIGH) {
+    myConnection.println("d2");
+    Serial.flush();
+  }
+
+  else myConnection.println("d2");
+  Serial.flush();
+}
+
+
+delay(10);
+// delay in between reads for stability
+}
